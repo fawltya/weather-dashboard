2957b19b4b56cdcba89935d5a434a1e79efae303
Seamus Barnett <seamus0689@gmail.com>
2023-01-31 23:46:43 +0000

    Previous searches stored in local Storage and working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,40 @@
 var lon;
 var lat;
 var now = luxon.DateTime.now();
-console.log(now);
 var apiKey = "732b53dac3ec24d2df14a0698f84453c";
 var j = 0;
-
+var locationQueryURL;
+var input;
+var queryURL;
 var recentSearches = [];
 
 $("#search-button").on("click", function (e) {
   e.preventDefault();
   input = $("#search-input").val().trim();
-  recentSearches.push(input);
+  var retrievedSearches = JSON.parse(localStorage.getItem("recentSearches"));
+  if (!retrievedSearches) retrievedSearches = [];
+  recentSearches = retrievedSearches.concat([input]);
+  localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  //   recentSearches.push(input);
+  //   localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  //   console.log(recentSearches);
   runSearch(input);
+  $("#prev-searches").empty();
+  previousButtons();
+
+  //   $("#search-input").empty(); // not working
 });
 
 function runSearch(input) {
   /** Get search input query */
-
-  var locationQueryURL = `https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${apiKey}`;
+  //   $("#today").empty();
+  $("#today").empty();
+  $("#forecast").empty();
+  locationQueryURL = `https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${apiKey}`;
 
   // Clear prev
-  $("#today").empty();
-  $("#search-input").empty(); // not working
+  //   $("#today").empty();
+  //   $("#forecast").empty();
   // Search for city and convert to lon/lat
   $.ajax({
     url: locationQueryURL,
@@ -30,7 +43,7 @@ function runSearch(input) {
     lon = result[0].lon;
     lat = result[0].lat;
 
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     $.ajax({
       url: queryURL,
@@ -61,46 +74,68 @@ function runSearch(input) {
         currentIconElement
       );
 
+      //   $("#forecast").empty(); // Clear previous data
       for (j = 8; j < 41; j += 8) {
-        var currentTemperature = result.list[j].main.temp - 272.15;
+        var Temperature = result.list[j].main.temp - 272.15;
         //   console.log(currentTemperature.toFixed(2));
-        var currentHumidity = result.list[j].main.humidity;
-        var currentWindSpeed = result.list[j].wind.speed;
-        var currentIconCode = result.list[j].weather[0].icon;
+        var Humidity = result.list[j].main.humidity;
+        var WindSpeed = result.list[j].wind.speed;
+        var IconCode = result.list[j].weather[0].icon;
+        var day = j / 8;
 
-        currentTemperatureElement = `<p>${currentTemperature.toFixed(2)}C</p>`;
-        currentHumidityElement = `<p>Humidity: ${currentHumidity}</p>`;
-        currentWindSpeedElement = `<p>Wind Speed: ${currentWindSpeed}</p>`;
-        currentIconElement = `<img src="https://openweathermap.org/img/w/${currentIconCode}.png" alt="weather icon" width="50">`;
-        var forecastElements = `<div class="forecast-div w-25 p-3">${currentTemperatureElement}
-            ${currentHumidityElement}
-            ${currentWindSpeedElement}
-            ${currentIconElement}`;
+        TemperatureElement = `<p>${Temperature.toFixed(2)}C</p>`;
+        HumidityElement = `<p>Humidity: ${Humidity}</p>`;
+        WindSpeedElement = `<p>Wind Speed: ${WindSpeed}</p>`;
+        IconElement = `<img src="https://openweathermap.org/img/w/${IconCode}.png" alt="weather icon" width="50">`;
+        var forecastElements = `<div class="forecast-div w-25 p-3">
+            +${day} day
+            ${TemperatureElement}
+            ${HumidityElement}
+            ${WindSpeedElement}
+            ${IconElement}`;
         // forecastElements.append(
         //   currentTemperatureElement,
         //   currentHumidityElement,
         //   currentWindSpeedElement,
         //   currentIconElement
         // );
+        // $("#forecast").empty();
         $("#forecast").append(forecastElements);
       }
     });
   });
 }
+previousButtons();
+// $(document).ready(function () {
 
+// });
 function previousButtons() {
-  //   previousButton.empty();
-  for (let i = 0; i < recentSearches.length; i++) {
-    // var previousButton = `<button class="search-btn" data-name="${recentSearches[i]}>${recentSearches[i]}</button>`;
-    // console.log(previousButton);
-    // previousButton.appendTo($("#history"));
-    $("#history").append(
-      `<button class="search-btn" data-name="${recentSearches[i]}>${recentSearches[i]}</button>`
-    );
+  //   $("#history").empty();
+  var recentSearches = JSON.parse(
+    localStorage.getItem("recentSearches", input)
+  );
 
-    // $("#history").setHTML(previousButton);
+  for (let i = 0; i < recentSearches.length; i++) {
+    var prevListItem = $(
+      `<i class="search-btn" data-name="${recentSearches[i]}">`
+    );
+    prevListItem.text(`${recentSearches[i]}`);
+
+    $("#prev-searches").append(prevListItem);
   }
 }
+if ($(".search-btn")) {
+  $("#clear-searches-btn").on("click", function (event) {
+    event.preventDefault();
+    localStorage.removeItem("recentSearches"); // Only removes highscore local storage data to keep dark mode localstorage
+    $("#prev-searches").empty();
+  });
+}
 
-// previousButtons();
+// $(".search-btn").on("click", function (e) {
+//   e.preventDefault();
+//   runSearch($("data-name"));
+// });
+
+// $(document).on("click", ".search-btn", runSearch($("data-name")));
 // $(document).on("click", ".search-btn", runSearch($("data-name")));
